75a1c54a5780e0c8623dfab22cabd1ad00c7d494
raz001 <razbhagat1@gmail.com>
2023-11-25 18:58:12 +0530

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
-
+import './App.css'
+import Navbar from './components/Navbar/Navbar';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='relative z-50'>
+        <div className='absolute'>
+          <Navbar />
+        </div>
+      </div>
+      <div>
+        <div className='overflow-hidden'>
+          <div className='relative h-screen'>
+            <img className='absolute top-0 transition ease-in duration-200 max-xl:right-[-1050px] xl:right-[-970px] -z-30 image' width='4377' src="https://github.githubassets.com/images/modules/site/home-campaign/hero-bg.webp" alt="" />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
