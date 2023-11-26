d4b00d94baa26ba47738f7e9325a219f853d4a4d
raz001 <razbhagat1@gmail.com>
2023-11-26 22:24:50 +0530

    body part added-hero, productivity, security...

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import './App.css'
 import Navbar from './components/Navbar/Navbar';
+import Hero from './components/HeroSection/Hero'
+import Productivity from './components/Productivity/Productivity';
+import Security from './components/Security/Securuty';
 function App() {
   return (
     <div className="App">
@@ -10,8 +13,17 @@ function App() {
       </div>
       <div>
         <div className='overflow-hidden'>
-          <div className='relative h-screen'>
+          <div className='relative'>
             <img className='absolute top-0 transition ease-in duration-200 max-xl:right-[-1050px] xl:right-[-970px] -z-30 image' width='4377' src="https://github.githubassets.com/images/modules/site/home-campaign/hero-bg.webp" alt="" />
+          </div>
+          <div className='hero-section px-3'>
+            <Hero />
+          </div>
+          <div id='productivity' className='home-campaign-productivity px-4 pt-8 overflow-hidden'>
+            <Productivity />
+          </div>
+          <div id='security' className='home-campaign-productivity px-4 pt-8 pb-16 overflow-hidden'>
+            <Security />
           </div>
         </div>
       </div>
